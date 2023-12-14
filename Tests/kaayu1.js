980dd1c22a7afe1bfb72cda69cb18186a13bd7e9
sushobhan1993 <sensushobhan21@gmail.com>
2023-12-14 14:08:20 +0530

    14th Dec

--- Tests/kaayu1.js
+++ Tests/kaayu1.js
@@ -1,15 +1,16 @@
 fixture('Getting Started').page('http://15.156.242.8/login');
 
+//fixture('Getting Started').page('https://admin.kaayu.ca/login').skipJsErrors(true);
+
+
+
 test('1st test',async t => {
 
     //For class we use .   for id we use #
     await t.typeText('#username' , 'sushobhan123')
-    await t.typeText('#password' , 'PAssword@1234')
+    await t.typeText('#password' , 'Password@1234')
     await t.click('body > div > div > div:nth-child(2) > div > div > div > div.col-lg-6.d-flex.justify-content-center.align-items-center > div > form > button')
    // await t.pressKey ('enter')
-    
-    
-
 
 })
 
@@ -18,5 +19,6 @@ test('1st test',async t => {
 
 test('2nd test',async t => {
 
+    await t.typeText('#username' , 'sushobhan123')
     
 })
