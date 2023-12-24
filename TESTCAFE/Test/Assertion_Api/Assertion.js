8dad0a6b87d31ee4ff1e6f93fbaeef3bdd708e47
sushobhan1993 <sensushobhan21@gmail.com>
2023-12-24 16:31:12 +0530

    Assertion added

--- TESTCAFE/Test/Assertion_Api/Assertion.js
+++ TESTCAFE/Test/Assertion_Api/Assertion.js
@@ -1,9 +1,32 @@
 import { Selector } from 'testcafe';
 
-fixture('Assertion Api').page('https://demoqa.com');
+fixture('Assertion Api').page('https://demoqa.com/elements').skipJsErrors(true);
 
+//Check how many left container are there , match that 6 containers are present or not
+test ('Deep Equal', async t => {
 
-test ('Deep qual', async t => {
+    await t.expect(Selector('.header-wrapper').count).eql (6, "Total item count" ,{ timeout: 5000});
+
 
 }
 )
+//check for not equal 
+test ('Not Deep Equal', async t => {
+
+    await t.expect(Selector('.header-wrapper').count).notEql (5, "Total item count" ,{ timeout: 5000});
+
+
+}
+)
+//check the element that present or not 
+test ('ok', async t =>{
+    await t.click ('#item-0');
+    await t.expect(Selector('#submit').exists).ok('The submit button are there');
+
+})
+//check the element that not present
+test ('Notok', async t =>{
+    
+    await t.expect(Selector('#submit').exists).notOk('The submit button are there');
+
+})
